28520b656801e1fe915f12bf6b3efba9ebbbdd52
ruu <wjrdjs005@naver.com>
2024-07-31 20:03:49 +0900

    fix & refactor: 코치님 피드백 반영해서 모바일 UI로 변경 및 App.js 코드 리팩토링

--- src/components/Layout/DefaultLayout.jsx
+++ src/components/Layout/DefaultLayout.jsx
@@ -1,28 +1,28 @@
-import {Container, Grid} from "@mui/material";
+import { Container, Grid, styled } from "@mui/material";
 import Header from "./Header.jsx";
 
-const DefaultLayout = ({children}) => {
+const StyledDiv = styled(Container)(({ theme }) => ({
+  maxWidth: "600px",
+}));
 
-    return (
-        <>
-            <Header />
-            <Container style={{width: '100%', marginTop: '3.25rem'}} >
-                <Grid
-                    container
-                    spacing={2}
-                    alignItems="flex-start"
-                    justifyContent="flex-start"
-                >
-                    <Grid
-                        item
-                        xs={12}
-                        sm={12}>
-                        {children}
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    )
-}
+const DefaultLayout = ({ children }) => {
+  return (
+    <StyledDiv>
+      <Header />
+      <Container style={{ maxWidth: "600px", marginTop: "3.25rem" }}>
+        <Grid
+          container
+          spacing={2}
+          alignItems="flex-start"
+          justifyContent="flex-start"
+        >
+          <Grid item xs={12} sm={12}>
+            {children}
+          </Grid>
+        </Grid>
+      </Container>
+    </StyledDiv>
+  );
+};
 
 export default DefaultLayout;
