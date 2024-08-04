efc14a0a7c6ba84bd059e7bbd18863414f152152
[Cloud3] 이혁 <hyuk3852@gmail.com>
2024-08-04 23:58:13 +0900

    Feat 모달창에 X버튼으로 닫히게 구현중

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,21 +1,17 @@
-import { Container } from '@mui/material';
+import { Box, Container, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import './Modal.css';
 import { Carousel } from './Carousel';
 
-export const Modal = ({
-    isOpen,
-    toggleOpen,
-}) => (
-    <Container style={{ maxWidth: '600px' ,textAlign: 'center', marginTop: '3rem' }} >
-    <div
-        className={`overlay ${
-            isOpen ? "open" : ""}`
-        }
-        onClick={toggleOpen}
-        >
-            <div className='modal'>
-                <Carousel></Carousel>
+export const Modal = ({ isOpen, toggleOpen }) => (
+    <Container style={{ maxWidth: '600px', textAlign: 'center', marginTop: '3rem' }}>
+        <div className={`overlay ${isOpen ? "open" : ""}`} onClick={toggleOpen}>
+            <div className='modal' onClick={e => e.stopPropagation()}>
+                    <IconButton className='close-button' onClick={toggleOpen}>
+                        <CloseIcon style={{marginTop: '0.5rem'}}/>
+                    </IconButton>
+                <Carousel />
             </div>
-    </div>
+        </div>
     </Container>
 );
