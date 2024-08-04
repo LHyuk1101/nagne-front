113b7651ce9b4032bd69350f58e02efeff4d9bba
[Cloud3] 이혁 <hyuk3852@gmail.com>
2024-08-04 21:32:14 +0900

    Feat CreatePlan 달력에서 최대 10일까지만 선택가능하게 범위축소, 선택 후 날짜변경도 가능

--- src/pages/CreatePlan.jsx
+++ src/pages/CreatePlan.jsx
@@ -2,15 +2,14 @@ import { Container, Typography, Box, Button, ThemeProvider } from '@mui/material
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useState } from "react";
-import { addMonths, differenceInDays } from 'date-fns';
+import { addDays, addMonths } from 'date-fns';
 import '../assets/styles/CreatePlan.css';
 import { Modal } from '../components/UI/Modal';
-import { theme } from '../assets/styles/globalStyle'
+import { theme } from '../assets/styles/globalStyle';
 
 const CreatePlan = () => {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
-    const [error, setError] = useState("");
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = () => setIsOpen(!isOpen);
@@ -19,61 +18,53 @@ const CreatePlan = () => {
         const [start, end] = dates;
         setStartDate(start);
         setEndDate(end);
-
-        if (start && end) {
-            const dayDifference = differenceInDays(end, start);
-            if (dayDifference > 9) {
-                setError("Please select a date less than 10 days");
-            } else {
-                setError("");
-            }
-        } else {
-            setError("");
-        }
     };
 
-    const isNextButtonDisabled = !startDate || !endDate || error !== "";
+    const isNextButtonDisabled = !startDate || !endDate;
+
+    const filterDate = (date) => {
+        if (!startDate) return date;
+        if (!endDate) {
+            return date >= startDate && date <= addDays(startDate, 9);
+        }
+        return true; // endDate가 선택된 후에는 모든 날짜를 활성화
+    };
 
     return (
         <ThemeProvider theme={theme}>
-        <Container style={{ maxWidth: '600px', textAlign: 'center', marginTop: '3rem' }}>
-            <Typography variant="h4" gutterBottom>
-                Please select a travel date
-            </Typography>
-            <Box display="flex" justifyContent="center" mt={2}>
-                <DatePicker
-                    selected={startDate}
-                    onChange={onChange}
-                    minDate={new Date()}
-                    maxDate={addMonths(new Date(), 5)}
-                    startDate={startDate}
-                    endDate={endDate}
-                    selectsRange
-                    inline
-                    showDisabledMonthNavigation
-                    calendarStartDay={1}
-                />
-            </Box>
-            {error && (
-                <Typography variant="body2" color="error" style={{ marginTop: '3rem' }}>
-
-                    {error}
+            <Container style={{ maxWidth: '600px', textAlign: 'center', marginTop: '3rem' }}>
+                <Typography variant="h4" gutterBottom>
+                    Please select a travel date
                 </Typography>
-            )}
-            <Button 
-                variant="contained" 
-                color="primary" 
-                style={{ marginTop: '3rem' }} 
-                disabled={isNextButtonDisabled}
-                onClick={toggleOpen}
-            >
-                NEXT
-            </Button>
-            <Modal 
-                isOpen={isOpen}
-                toggleOpen={toggleOpen}
-            />
-        </Container>
+                <Box display="flex" justifyContent="center" mt={2}>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={onChange}
+                        minDate={new Date()}
+                        maxDate={addMonths(new Date(), 5)}
+                        startDate={startDate}
+                        endDate={endDate}
+                        selectsRange
+                        inline
+                        showDisabledMonthNavigation
+                        calendarStartDay={1}
+                        filterDate={filterDate} // 추가된 필터링 함수
+                    />
+                </Box>
+                <Button 
+                    variant="contained" 
+                    color="primary" 
+                    style={{ marginTop: '3rem' }} 
+                    disabled={isNextButtonDisabled}
+                    onClick={toggleOpen}
+                >
+                    NEXT
+                </Button>
+                <Modal 
+                    isOpen={isOpen}
+                    toggleOpen={toggleOpen}
+                />
+            </Container>
         </ThemeProvider>
     );
 };
