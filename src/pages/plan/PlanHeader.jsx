abc6646f4c938f27ca5f9f58e1d9262263481e4b
ruu <wjrdjs005@naver.com>
2024-08-06 23:27:38 +0900

    refector: PlanHeader 스타일 컴포넌트 외부로 분리

--- src/pages/plan/PlanHeader.jsx
+++ src/pages/plan/PlanHeader.jsx
@@ -1,52 +1,12 @@
-import { Box, styled, Typography } from "@mui/material";
-
-const Header = styled(Box)(({ theme }) => ({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  minHeight: 120,
-  backgroundColor: "#ffffff",
-  padding: theme.spacing(3),
-  boxSizing: "border-box",
-  width: "100%",
-}));
-
-const HeaderColumn = styled(Box)({
-  display: "flex",
-  alignItems: "center",
-});
-
-const DestinationColumn = styled(HeaderColumn)({
-  flex: "0 0 35%",
-});
-
-const DateColumn = styled(HeaderColumn)({
-  flex: "0 0 40%",
-});
-
-const EmptyColumn = styled(HeaderColumn)({
-  flex: "0 0 10%",
-});
-
-const Destination = styled(Typography)(({ theme }) => ({
-  fontSize: 32,
-  fontWeight: 700,
-  color: "#464555",
-  letterSpacing: "-0.5px",
-}));
-
-const DateRange = styled(Box)({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "flex-start",
-});
-
-const DateText = styled(Typography)(({ theme }) => ({
-  fontSize: 20,
-  color: "#464555",
-  letterSpacing: "0.25px",
-  alignSelf: "center",
-}));
+import {
+  DateColumn,
+  DateText,
+  Destination,
+  DestinationColumn,
+  EmptyColumn,
+  Header,
+  DateRange,
+} from "./PlanHeader.style.jsx";
 
 const PlanHeader = () => {
   return (
---
+++ src/pages/plan/PlanHeader.style.jsx
@@ -0,0 +1,49 @@
+import { Box, styled, Typography } from "@mui/material";
+
+export const Header = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  minHeight: 120,
+  backgroundColor: "#ffffff",
+  padding: theme.spacing(3),
+  boxSizing: "border-box",
+  width: "100%",
+}));
+
+export const HeaderColumn = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+});
+
+export const DestinationColumn = styled(HeaderColumn)({
+  flex: "0 0 35%",
+});
+
+export const DateColumn = styled(HeaderColumn)({
+  flex: "0 0 40%",
+});
+
+export const EmptyColumn = styled(HeaderColumn)({
+  flex: "0 0 10%",
+});
+
+export const Destination = styled(Typography)(({ theme }) => ({
+  fontSize: 32,
+  fontWeight: 700,
+  color: "#464555",
+  letterSpacing: "-0.5px",
+}));
+
+export const DateRange = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+});
+
+export const DateText = styled(Typography)(({ theme }) => ({
+  fontSize: 20,
+  color: "#464555",
+  letterSpacing: "0.25px",
+  alignSelf: "center",
+}));
