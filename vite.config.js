bdf1e12b897c0e764bcea216c12f7d897088d744
ruu <wjrdjs005@naver.com>
2024-08-08 12:58:30 +0900

    feat: 프로덕션 빌드용 환경 설정및 스크립트 추가

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.js'],
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    test: {
+      environment: "jsdom",
+      setupFiles: ["./vitest.setup.js"],
+    },
+    define: {
+      "import.meta.env.VITE_API_KEY": JSON.stringify(env.VITE_API_KEY),
+    },
+  };
+});
