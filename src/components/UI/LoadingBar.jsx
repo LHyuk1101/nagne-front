d2d7048f292e93b49b979a198ad16926b2679955
김동현 <ruggy245@naver.com>
2024-08-13 21:22:16 +0900

    nagne 로딩바 gif 추가

--- src/components/UI/LoadingBar.jsx
+++ src/components/UI/LoadingBar.jsx
@@ -7,12 +7,30 @@ import {
   CircularProgress,
 } from "@mui/material";
 
-const LoadingBar = ({ open, message }) => {
+const LoadingDialog = ({ open, message, gifSrc }) => {
   return (
     <Dialog open={open} aria-labelledby="loading-dialog-title">
       <DialogContent>
         <Box display="flex" flexDirection="column" alignItems="center" p={3}>
-          <CircularProgress size={60} />
+          <Box position="relative" display="inline-flex">
+            <CircularProgress size={60} />
+            <Box
+              position="absolute"
+              top={0}
+              left={0}
+              bottom={0}
+              right={0}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <img
+                src={gifSrc}
+                alt="Loading..."
+                style={{ width: 40, height: 40 }}
+              />
+            </Box>
+          </Box>
           <Typography variant="h6" style={{ marginTop: 20 }}>
             {message || "Loading..."}
           </Typography>
@@ -25,4 +43,4 @@ const LoadingBar = ({ open, message }) => {
   );
 };
 
-export default LoadingBar;
+export default LoadingDialog;
