a1d9fc8395306ef2c9729e3a5fb94ef19a388d5c
김동현 <ruggy245@naver.com>
2024-08-13 20:44:45 +0900

    merge feature-place into feat/plan1

--- src/pages/plan/PlanFirst.jsx
+++ src/pages/plan/PlanFirst.jsx
@@ -1,39 +1,22 @@
-import { Delete as DeleteIcon } from "@mui/icons-material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import PlaceModal from "../place/PlaceModal.jsx";
 import LINKS from "../../routes/Links.jsx";
 import usePlanStore from "../../store/PlanContext.js";
 import { useSelectedPlaces } from "../../store/place/PlaceContext.jsx";
 import {
   StyledTab,
   StyledTabs,
-  ContentArea,
-  PlaceList,
-  PlaceHeader,
-  PlaceNumber,
-  PlaceName,
-  AddPlaceButton,
   ButtonContainer,
   CreateScheduleButton,
-  PlaceItem,
-  PlaceItemNumber,
-  PlaceItemContent,
-  PlaceItemName,
-  PlaceItemAddress,
-  PlaceImgContent,
-  PlaceImage,
-  PlaceItemActions,
 } from "./PlanFirst.style.jsx";
-import { IconColor } from "../../constants/constant.js";
-import IconButton from "@mui/material/IconButton";
+import PlaceTab from "../place/PlaceTab.jsx";
+import AccommodationTab from "../place/AccommodationTab.jsx";
 
 const PlanFirst = () => {
-  const { startDate, endDate, setSelectedPlaces } = usePlanStore();
+  const { startDate, endDate, placeName, setSelectedPlaces } = usePlanStore();
   const navigate = useNavigate();
   const [tabValue, setTabValue] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { selectedPlaces, removePlace } = useSelectedPlaces();
+  const { selectedPlaces } = useSelectedPlaces();
 
   // useEffect(() => {
   //   initRender();
@@ -57,45 +40,15 @@ const PlanFirst = () => {
 
   const renderRefreshNotification = () => {};
 
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
   const handleTabChange = (event, newValue) => {
+    console.log(newValue);
     setTabValue(newValue);
-  };
-
-  const handleDeleteItems = (e) => {
-    e.preventDefault();
   };
 
   const handleRedirectButton = (e) => {
     e.preventDefault();
     setSelectedPlaces(selectedPlaces);
     navigate(LINKS.PLAN.path);
-  };
-
-  const responseDataEx = {
-    startDay: "",
-    endDay: "",
-    place: [
-      {
-        name: "",
-        contentTypeId: "",
-        lat: "",
-        lng: "",
-        overview: "",
-      },
-    ],
-    맛집: [
-      {
-        name: "",
-        contentTypeId: "",
-        lat: "",
-        lng: "",
-        overview: "",
-      },
-    ],
   };
 
   return (
@@ -105,43 +58,8 @@ const PlanFirst = () => {
         <StyledTab label="Accommodation" />
       </StyledTabs>
 
-      <PlaceHeader>
-        <PlaceNumber>3</PlaceNumber>
-        <PlaceName>Reset</PlaceName>
-        <AddPlaceButton onClick={toggleModal}>+ Add Place</AddPlaceButton>
-      </PlaceHeader>
-      <ContentArea>
-        <PlaceList>
-          {selectedPlaces.map((item, index) => (
-            <PlaceItem key={item.id}>
-              <PlaceItemNumber
-                backgroundColor={IconColor[index % IconColor.length]}
-              >
-                {index + 1}
-              </PlaceItemNumber>
-              <PlaceImgContent>
-                <PlaceImage
-                  src={item.placeUrlImages[0] || "기본 이미지 URL"}
-                  alt={item.title}
-                  onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = "기본 이미지 URL"; // 여기에 기본 이미지 URL을 넣으세요
-                  }}
-                />
-              </PlaceImgContent>
-              <PlaceItemContent>
-                <PlaceItemName>{item.title}</PlaceItemName>
-                <PlaceItemAddress>{item.address}</PlaceItemAddress>
-              </PlaceItemContent>
-              <PlaceItemActions>
-                <IconButton size="small" onClick={() => removePlace(item.id)}>
-                  <DeleteIcon />
-                </IconButton>
-              </PlaceItemActions>
-            </PlaceItem>
-          ))}
-        </PlaceList>
-      </ContentArea>
+      {tabValue === 0 && <PlaceTab />}
+      {tabValue === 1 && <AccommodationTab />}
 
       <ButtonContainer>
         <CreateScheduleButton
@@ -151,8 +69,6 @@ const PlanFirst = () => {
           Create Schedule
         </CreateScheduleButton>
       </ButtonContainer>
-
-      <PlaceModal open={isModalOpen} onClose={toggleModal} />
     </>
   );
 };
--- src/services/place/place.js
+++ src/services/place/place.js
@@ -1,11 +1,13 @@
 import axiosInstance from "../common/axios.js";
 import { RESPONSE_STATUS_ERROR } from "../../constants/constant.js";
 
-const getPlaceByArea = async (areaCode, regions, page) => {
-  const response = await axiosInstance.get("/api/place?page=1&size=1000", {
+const getPlaceByArea = async (areaCode, regions, page = 1, size = 10) => {
+  const response = await axiosInstance.get("/api/place", {
     params: {
       regions,
       areaCode,
+      page,
+      size,
     },
   });
 
@@ -13,11 +15,22 @@ const getPlaceByArea = async (areaCode, regions, page) => {
     const error = new Error("An error occurred while fetching the place");
     error.code = response.status;
     error.info = await response;
+    throw error;
   }
 
   const { data } = response;
 
-  return data;
+  const items = data.items.placeList;
+  const totalCount = data.items.totalCount;
+
+  const hasNextPage = page * size < totalCount;
+
+  return {
+    items,
+    nextPage: hasNextPage ? page + 1 : undefined,
+    totalCount,
+    currentPage: page,
+  };
 };
 
 export { getPlaceByArea };
