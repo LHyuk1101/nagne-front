bec91434882ee450993cf666e804154fb221d6e4
ruu <wjrdjs005@naver.com>
2024-08-15 03:41:57 +0900

    fix: styles 관련 오류 수정

--- src/pages/plan/PlanHeader.jsx
+++ src/pages/plan/PlanHeader.jsx
@@ -5,23 +5,10 @@ import {
   DateText,
 } from "./PlanHeader.style.jsx";
 import { PLAN_HEADER_TITLE } from "../../constants/constant.js";
-import LINKS from "../../routes/Links.jsx";
 import usePlanStore from "../../store/PlanContext.js";
-import { useNavigate } from "react-router-dom";
 import { toDate, formatDate } from "../../utils/dateUtils.js";
 const PlanHeader = () => {
   const { startDate, endDate, placeName } = usePlanStore();
-  const navigate = useNavigate();
-
-  if (
-    startDate === undefined ||
-    endDate === undefined ||
-    placeName === undefined
-  ) {
-    navigate({
-      pathname: LINKS.CREATE.path,
-    });
-  }
 
   return (
     <Header>
