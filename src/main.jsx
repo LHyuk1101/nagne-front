927bb57ab373baab2d1fa4ef78344a92e3ddcbb1
김동현 <ruggy245@naver.com>
2024-08-15 13:04:17 +0900

    refector:main.jsx 롤백

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,12 @@ import Router from "./routes/index.jsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <Router />
-    </ThemeProvider>
-  </QueryClientProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
+    ,
+  </React.StrictMode>,
 );
